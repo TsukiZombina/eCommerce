a835d135ff34d9f7bffa0e5fcb8b6da13212ad1f
Ernesto GM <ernt26dev@gmail.com>
2016-12-02 15:13:26 -0600

    Añade interfaces DetalleOrden y Orden

--- src/java/mx/uam/azc/modelo/dao/Orden.java
+++ src/java/mx/uam/azc/modelo/dao/Orden.java
@@ -1,13 +1,11 @@
 package mx.uam.azc.modelo.dao;
 
-import mx.uam.azc.modelo.beans.Administrador;
-
 /**
  *
  * @author Zelzin Marcela Márquez Navarrete
  * @author Ernesto García Maldonado
  * @version 1.0
  */
-public interface AdministradorDAO extends DAO<Administrador, Long>{
+public interface Orden extends DAO<Orden, Long>{
     
 }
