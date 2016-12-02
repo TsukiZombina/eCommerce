8fafd22d35fbe7fae199ba28e39d71658597358e
Ernesto GM <ernt26dev@gmail.com>
2016-12-02 14:54:00 -0600

    Crea bean Orden

---
+++ src/java/mx/uam/azc/modelo/beans/Orden.java
@@ -0,0 +1,61 @@
+package mx.uam.azc.modelo.beans;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author Zelzin Marcela Márquez Navarrete
+ * @author Ernesto García Maldonado
+ * @version 1.0
+ */
+public class Orden {
+    private Long idOrden = null;
+    private Date fecha;
+    private Date hora;
+    private double total;
+    private Long idUsuario;
+
+    public Orden() {
+    }
+
+    public Long getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(Long idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
