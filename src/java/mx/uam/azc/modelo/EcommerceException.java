e1e163abca420e02ccc589eeffe7f3c529d6f090
Ernesto GM <ernt26dev@gmail.com>
2016-12-02 14:18:05 -0600

    Elimina 2 constructores clase EcommerceException

--- src/java/mx/uam/azc/modelo/EcommerceException.java
+++ src/java/mx/uam/azc/modelo/EcommerceException.java
@@ -9,12 +9,4 @@ public class EcommerceException extends Exception{
     public EcommerceException(String message) {
         super(message);
     }
-
-    public EcommerceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public EcommerceException(Throwable cause) {
-        super(cause);
-    }
 }
